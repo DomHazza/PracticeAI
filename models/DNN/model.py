7929dd2899e79a366fcef5c41c6e61e7f8d920ea
DomHazza <domfxh@gmail.com>
2024-09-28 08:19:11 +0100

    model training sorted

--- models/DNN/model.py
+++ models/DNN/model.py
@@ -1,1 +1,28 @@
 import tensorflow as tf
+import pandas as pd
+import numpy as np
+
+
+
+
+class DeepModel(tf.keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.flatten = tf.keras.layers.Flatten()
+        self.d1 = tf.keras.layers.Dense(128, activation='relu')
+        self.d2 = tf.keras.layers.Dense(10, activation='softmax')
+
+
+    def call(self, x):
+        x = self.flatten(x)
+        x = self.d1(x)
+        x = self.d2(x)
+        return x
+
+
+
+
+
+
+
+
